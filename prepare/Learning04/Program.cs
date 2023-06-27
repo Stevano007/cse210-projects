cac544aaed5f83dbb3a97ebc93e9e0de642f5d82
Stevano007 <39330823+Stevano007@users.noreply.github.com>
2023-06-27 10:34:30 -0500

    learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
-    }
+        Assignment myAssignment= new Assignment("Jane", "Math");
+        string myAssgn= myAssignment.GetSummary();
+        Console.WriteLine(myAssgn);
+        WritingAssignment myWritingAssignment= new WritingAssignment("Joseph", "Art", "The Mona Lisa");
+        string myWritingAssgn= myWritingAssignment.GetWritingInformation();
+        Console.WriteLine(myWritingAssgn);
+
+    }    
 }
---
+++ prepare/Learning04/assignments.cs
@@ -0,0 +1,27 @@
+public class Assignment{
+   private string _studentName;
+   private string _topic;
+
+   public void SetStudentName(string studentName){
+       _studentName=studentName;
+   }
+
+   public string GetStudentName(){
+       return _studentName;
+   }
+   public void SetTopic(string topic){
+       _topic=topic;
+   }
+
+   public string GetTopic(){
+       return _topic;
+   }
+
+   public Assignment(string studentName, string topic){
+       _studentName=studentName;
+       _topic=topic;
+   }
+   public string GetSummary(){
+        return $"{_studentName} is working on {_topic}";
+   }
+}
